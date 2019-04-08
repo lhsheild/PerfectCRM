2f0709f7f361e4d4b2c7b154a9703b248cd45a37
lhsheild <lhsheild@yahoo.com>
2019-04-08 23:44:22 +0800

    CRM登录注册

--- crm/views.py
+++ crm/views.py
@@ -3,10 +3,46 @@ from django.http import JsonResponse
 from django.views import View
 from django import forms
 from django.contrib import auth
+from crm.models import UserInfo
+from django.core.exceptions import ValidationError
 
 
 class UserForm(forms.Form):
-    pass
+    user = forms.CharField(min_length=5)
+    pwd = forms.CharField(min_length=5)
+    r_pwd = forms.CharField(min_length=5)
+    email = forms.EmailField(min_length=5)
+
+    def clean_user(self):
+        val = self.cleaned_data.get('user')
+        user = UserInfo.objects.filter(username=val).first()
+        if user:
+            raise ValidationError('用户名已使用！')
+        else:
+            return val
+
+    def clean_pwd(self):
+        val = self.cleaned_data.get('pwd')
+        if val.isdigit():
+            raise ValidationError('密码不能为纯数字！')
+        else:
+            return val
+
+    def clean(self):
+        pwd = self.cleaned_data.get('pwd')
+        r_pwd = self.cleaned_data.get('r_pwd')
+        if pwd and r_pwd and r_pwd != pwd:
+            raise ValidationError('两次密码不一致')
+        else:
+            return self.cleaned_data
+
+    def clean_email(self):
+        val = self.cleaned_data.get('email')
+        import re
+        if re.search(r'^[0-9a-zA-Z_]{0,19}@[0-9a-zA-Z]{1,13}\.[com,cn,net]{1,3}$', val):
+            return val
+        else:
+            raise ValidationError('请输入正确邮箱地址')
 
 
 def login(request):
